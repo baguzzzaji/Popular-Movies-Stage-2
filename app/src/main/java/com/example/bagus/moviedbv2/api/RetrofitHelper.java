9da87e08887060a982b78f4e7b9d4e23876919b4
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-09-15 11:47:42 +0700

    remove API

--- app/src/main/java/com/example/bagus/moviedbv2/api/RetrofitHelper.java
+++ app/src/main/java/com/example/bagus/moviedbv2/api/RetrofitHelper.java
@@ -11,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class RetrofitHelper {
 
-    public final static String API_KEY = "1bb03dcadd1803cf79af629648c59d38";
+    public final static String API_KEY = "use your own api";
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
     private static Retrofit retrofit = null;
 
